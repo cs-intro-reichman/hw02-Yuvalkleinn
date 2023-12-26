c330ef04981b4f46111625a83ff5a3562d65ea88
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2023-12-26 11:44:27 +0200

    Final Solution

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,14 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		// Parse the command-line argument as an integer
+		int number = Integer.parseInt(args[0]);
+
+		// Iterate through numbers from 1 to the given number and prints the divisors
+		for (int i = 1; i <= number; i++) {
+			if (number % i == 0) {
+				System.out.println(i);
+			}
+		}
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,23 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
+	public static void main(String[] args) throws Exception {
+
+		// Get the input string from the command-line argument and declare some args
+        String word = args[0];
+        String reverseWord = "";
+        int middleIndex = 0;
+
+        // Calculate the middle index of the string
+        middleIndex = (word.length()%2 == 0) ? (word.length()/2) : (word.length()/2) + 1;
+        
+        // Reverse the string
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reverseWord = reverseWord + word.charAt(i);
+        }
+        
+        // Print the reversed string and the middle char of the string
+        System.out.println(reverseWord);
+        System.out.println("The middle character is: " + word.charAt(middleIndex-1));
 	}
 }
