3faadb5ca2305a838a385a1c70e4d3e2d43ff987
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2023-12-27 12:02:05 +0200

    Final Solution

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,18 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+        // Generate the two integers
+        int num1 = (int) (Math.random()*(9)) + 1;
+        int num2 = (int) (Math.random()*(9)) + 1;
+
+        // Print the first random integer
+        System.out.print(num1 + " ");
+
+        // Generate and print subsequent random integers in a non-decreasing sequence
+        while (num2>=num1) {
+            System.out.print(num2 + " ");
+            num1 = num2;
+            num2 = (int) (Math.random()*(9)) + 1;
+        }
 	}
 }
