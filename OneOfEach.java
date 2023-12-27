eec08886c50208debc1de24bb2e724078d2467a4
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2023-12-27 20:10:10 +0200

    Final Solution

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,34 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+
+		// Initialize variables
+		String str = "";
+		boolean isGirl = false;
+		boolean isBoy = false;
+		int howManyChildren = 0;
+		
+		// Continue looping until at least one child of each gender is born
+		while((isGirl && isBoy) != true) {
+			
+			// Generate a random number to represent the gender of the child
+			double randomGender = Math.random();
+			
+			// Check if the random number represents a girl (less than 0.5)
+			if(randomGender < 0.5) {
+				isGirl = true;
+				str += "g ";
+			}
+
+			else { 
+				isBoy = true;
+				str += "b ";
+			}
+			howManyChildren++; // Increment the total number of children
+		}
+		
+		// Output the string representing the gender sequence and the total number of children
+		System.out.println(str);
+		System.out.println("You made it... and you now have " + howManyChildren + " children.");
 	}
 }
