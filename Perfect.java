c038650c1db294938974980dd47195f77af470ca
yuvalkleinn <yuval.klein01@runi.post.ac.il>
2023-12-27 12:42:57 +0200

    Final Solution

--- Perfect.java
+++ Perfect.java
@@ -2,7 +2,31 @@
  *  Gets a command-line argument (int), and chekcs if the given number is perfect.
  */
 public class Perfect {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+	public static void main(String[] args) throws Exception {
+        
+        // Parse the command-line argument as an integer and declare some args
+        int num = Integer.parseInt(args[0]);
+        int sum = 0;
+        String divisor = "";
+
+        // Iterate through potential divisors up to (num - 1) and check if i is a divisor of num
+        for (int i=1; i<num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                divisor += i + " + ";
+            }
+        }
+
+        // Remove the trailing " + " from the divisor string
+        divisor = divisor.substring(0, divisor.length()-3);
+
+        // Check if the sum of divisors equals the original number
+        if (sum == num) {
+            System.out.print(num + " is a perfect nubmer since " + num + " = " + divisor);
+        }
+        else {
+            System.out.println(num + " is not a perfect number");
+        }
+    }
 }
+
